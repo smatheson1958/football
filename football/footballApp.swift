60b0b92ab0cff34d4696897aa25b69c0fa3282b8
scottmatheson58 <scott@matheson.it>
2026-03-13 16:40:58 +0000

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by scott matheson on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
